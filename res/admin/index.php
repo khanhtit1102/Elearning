62b167b6eda71393501f7ab8da53360fc3a0fb5a
Nong Van Khanh <khanhnongvan0@gmail.com>
2021-05-20 14:22:28 +0700

    Change DB

--- res/admin/index.php
+++ res/admin/index.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta content='Học trực tuyến cùng với những Giảng viên hàng đầu. Học online 24/7 - Tự tin làm chủ tương lai. Siêu thị bài giảng trực tuyến lớn nhất Việt Nam' name='description'>
-	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('res/') ?>imgs/icon.png">
-	<title>Trang Quản Lý Của ADMIN - Edumall</title>
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>/res/courses/assets/img/favicon.ico">
+	<title>Trang Quản Lý Của ADMIN - Courses</title>
 	<link rel="stylesheet" href="<?php echo base_url('res/') ?>bs/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo base_url('res/') ?>awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?php echo base_url('res/') ?>css/morris.css">
